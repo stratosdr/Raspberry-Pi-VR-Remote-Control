634a1ad82bca91cb5bcf4377418697962b849738
stratosdr <104944061+stratosdr@users.noreply.github.com>
2022-06-02 01:50:05 +0300

    Add files via upload

---
+++ GVRButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GVRButton : MonoBehaviour
+{
+    public Image imgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 0.2f;
+    bool gvrStatus;
+    public float gvrTimer = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgCircle.fillAmount = gvrTimer / totalTime;
+        }
+        if (gvrTimer < totalTime)
+        {
+            GVRClick.Invoke();
+        }
+    }
+    public void GvrOn()
+    {
+        gvrStatus = true;
+    }
+    public void GvrOff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgCircle.fillAmount = 0;
+    }
+}
